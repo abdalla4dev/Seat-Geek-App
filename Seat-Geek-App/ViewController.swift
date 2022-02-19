c56431c5ec90955e5ec5e5855461bf1d01f86d0d
Abdallah ragab <dev.abdallah.ragb@gmail.com>
2022-02-19 14:56:02 +0200

    init project

---
+++ Seat-Geek-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Seat-Geek-App
+//
+//  Created by abdallah ragab on 19/02/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
